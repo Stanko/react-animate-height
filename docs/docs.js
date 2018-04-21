c7b6e10cdeae2767b7ea5f0a167efebd9e954da6
Tobias Harison-Denby <tobiaswhd@gmail.com>
2018-04-21 11:48:07 +0100

    feat: allow transitions to be delayed (#51)
    This commit adds a `delay` prop to the component which is the time, in milliseconds, to offset the
    transition animation. It applies this time to the transition styles, and adds it to the duration to
    form the time in which to run animation callbacks.

--- docs/docs.js
+++ docs/docs.js
@@ -11,6 +11,7 @@ const Example = class extends React.Component {
     this.state = {
       height: 0,
       height2: 'auto',
+      height3: 0,
     };
   }
 
@@ -18,6 +19,8 @@ const Example = class extends React.Component {
     const {
       height,
       height2,
+      height3,
+      delay,
     } = this.state;
 
     return (
@@ -128,7 +131,7 @@ const Example = class extends React.Component {
             100% (of the parent height)
           </button>
         </div>
-        <div style={ { height: 800 } }>
+        <div>
           <AnimateHeight
             height={ height2 }
             duration={ 500 }
@@ -182,6 +185,98 @@ const Example = class extends React.Component {
             </div>
           </AnimateHeight>
         </div>
+
+        <h3>Demo, with transition delay</h3>
+        <p>
+          Current Height: <b>{ height3 }</b><br/>
+          Current Delay: <b>{ delay }</b>
+        </p>
+        <p>Set delay to:</p>
+        <div className='buttons'>
+          <button className='btn btn-sm' onClick={ () => this.setState({ delay: 0 }) }>
+            0
+          </button>
+          <button className='btn btn-sm' onClick={ () => this.setState({ delay: 300 }) }>
+            300
+          </button>
+          <button className='btn btn-sm' onClick={ () => this.setState({ delay: 600 }) }>
+            600
+          </button>
+          <button className='btn btn-sm' onClick={ () => this.setState({ delay: 1000 }) }>
+            1000
+          </button>
+        </div>
+        <p className=''>Set height to:</p>
+        <div className='buttons'>
+          <button className='btn btn-sm' onClick={ () => this.setState({ height3: 0 }) }>
+            0
+          </button>
+          <button className='btn btn-sm' onClick={ () => this.setState({ height3: 100 }) }>
+            100
+          </button>
+          <button className='btn btn-sm' onClick={ () => this.setState({ height3: 200 }) }>
+            200
+          </button>
+          <button className='btn btn-sm' onClick={ () => this.setState({ height3: 300 }) }>
+            300
+          </button>
+          <button className='btn btn-sm' onClick={ () => this.setState({ height3: 'auto' }) }>
+            auto
+          </button>
+        </div>
+        <AnimateHeight
+          height={ height3 }
+          delay={delay}
+          duration={ 500 }
+          onAnimationEnd={ () => { console.log('React Animate Height - animation ended'); } }
+          onAnimationStart={ () => { console.log('React Animate Height - animation started'); } }
+          className='demo demo-3'
+          animateOpacity
+        >
+          <div className='demo-content'>
+            <p>
+              It looked serious, but we in California, like everywhere else, were
+              not alarmed. We were sure that the bacteriologists would find a way to
+              overcome this new germ, just as they had overcome other germs in the
+              past. But the trouble was the astonishing quickness with which this germ
+              destroyed human beings, and the fact that it inevitably killed any
+              human body it entered. No one ever recovered. There was the old Asiatic
+              cholera, when you might eat dinner with a well man in the evening, and
+              the next morning, if you got up early enough, you would see him being
+              hauled by your window in the death-cart. But this new plague was quicker
+              than that&mdash;much quicker.
+            </p>
+            <input
+              className='form-control'
+              type='text'
+              placeholder='Test for focus'
+              onFocus={ () => console.log('Input 2 focused') }
+            />
+            <p>
+              From the moment of the first signs of it, a man would be dead in an
+              hour. Some lasted for several hours. Many died within ten or fifteen
+              minutes of the appearance of the first signs.
+            </p>
+            <p>
+              The heart began to beat faster and the heat of the body to increase.
+              Then came the scarlet rash, spreading like wildfire over the face and
+              body. Most persons never noticed the increase in heat and heart-beat,
+              and the first they knew was when the scarlet rash came out. Usually,
+              they had convulsions at the time of the appearance of the rash. But
+              these convulsions did not last long and were not very severe. If one
+              lived through them, he became perfectly quiet, and only did he feel a
+              numbness swiftly creeping up his body from the feet. The heels became
+              numb first, then the legs, and hips, and when the numbness reached
+              as high as his heart he died. They did not rave or sleep. Their minds
+              always remained cool and calm up to the moment their heart numbed and
+              stopped. And another strange thing was the rapidity of decomposition. No
+              sooner was a person dead than the body seemed to fall to pieces, to
+              fly apart, to melt away even as you looked at it. That was one of the
+              reasons the plague spread so rapidly. All the billions of germs in a
+              corpse were so immediately released.
+            </p>
+          </div>
+        </AnimateHeight>
       </div>
     );
   }
