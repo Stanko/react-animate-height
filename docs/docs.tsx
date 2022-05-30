752ff30b318e781ec8dae2f4ea00a7c36024de8a
Stanko <stanko.tadic@gmail.com>
2022-05-30 21:25:18 +0200

    Docs

--- docs/docs.tsx
+++ docs/docs.tsx
@@ -3,11 +3,62 @@ import { createRoot } from 'react-dom/client';
 
 import AnimateHeight, { Height } from '../src/index';
 
+const DemoContent = ({ index }) => {
+  return (
+    <div className="demo-content">
+      <p>
+        It looked serious, but we in California, like everywhere else, were not
+        alarmed. We were sure that the bacteriologists would find a way to
+        overcome this new germ, just as they had overcome other germs in the
+        past. But the trouble was the astonishing quickness with which this germ
+        destroyed human beings, and the fact that it inevitably killed any human
+        body it entered. No one ever recovered. There was the old Asiatic
+        cholera, when you might eat dinner with a well man in the evening, and
+        the next morning, if you got up early enough, you would see him being
+        hauled by your window in the death-cart. But this new plague was quicker
+        than that&mdash;much quicker.
+      </p>
+      <input
+        className="form-control"
+        type="text"
+        placeholder="Focus test"
+        onFocus={() => console.log(`Input ${index} focused`)}
+      />
+      <p>
+        From the moment of the first signs of it, a man would be dead in an
+        hour. Some lasted for several hours. Many died within ten or fifteen
+        minutes of the appearance of the first signs.
+      </p>
+      <p>
+        The heart began to beat faster and the heat of the body to increase.
+        Then came the scarlet rash, spreading like wildfire over the face and
+        body. Most persons never noticed the increase in heat and heart-beat,
+        and the first they knew was when the scarlet rash came out. Usually,
+        they had convulsions at the time of the appearance of the rash. But
+        these convulsions did not last long and were not very severe. If one
+        lived through them, he became perfectly quiet, and only did he feel a
+        numbness swiftly creeping up his body from the feet. The heels became
+        numb first, then the legs, and hips, and when the numbness reached as
+        high as his heart he died. They did not rave or sleep. Their minds
+        always remained cool and calm up to the moment their heart numbed and
+        stopped. And another strange thing was the rapidity of decomposition. No
+        sooner was a person dead than the body seemed to fall to pieces, to fly
+        apart, to melt away even as you looked at it. That was one of the
+        reasons the plague spread so rapidly. All the billions of germs in a
+        corpse were so immediately released.
+      </p>
+    </div>
+  );
+};
+
 const Example = () => {
   const [height1, setHeight1] = useState<Height>(0);
   const [height2, setHeight2] = useState<Height>('auto');
   const [height3, setHeight3] = useState<Height>('auto');
   const [delay, setDelay] = useState<number>(0);
+
+  const options: Height[] = [0, 100, 200, 300, 'auto'];
+  const delays: number[] = [0, 300, 600, 1000];
 
   return (
     <div>
@@ -17,67 +68,20 @@ const Example = () => {
       </p>
       <p className="">Set height to:</p>
       <div className="buttons">
-        <button className="btn btn-sm" onClick={() => setHeight1(0)}>
-          0
-        </button>
-        <button className="btn btn-sm" onClick={() => setHeight1(100)}>
-          100
-        </button>
-        <button className="btn btn-sm" onClick={() => setHeight1(200)}>
-          200
-        </button>
-        <button className="btn btn-sm" onClick={() => setHeight1(300)}>
-          300
-        </button>
-        <button className="btn btn-sm" onClick={() => setHeight1('auto')}>
-          auto
-        </button>
+        {options.map((option) => {
+          const isActive = height1 === option;
+          return (
+            <button
+              className={`btn btn-sm ${isActive ? 'btn-selected' : ''}`}
+              onClick={() => setHeight1(option)}
+            >
+              {option}
+            </button>
+          );
+        })}
       </div>
       <AnimateHeight height={height1} className="demo demo-1">
-        <div className="demo-content">
-          <p>
-            It looked serious, but we in California, like everywhere else, were
-            not alarmed. We were sure that the bacteriologists would find a way
-            to overcome this new germ, just as they had overcome other germs in
-            the past. But the trouble was the astonishing quickness with which
-            this germ destroyed human beings, and the fact that it inevitably
-            killed any human body it entered. No one ever recovered. There was
-            the old Asiatic cholera, when you might eat dinner with a well man
-            in the evening, and the next morning, if you got up early enough,
-            you would see him being hauled by your window in the death-cart. But
-            this new plague was quicker than that&mdash;much quicker.
-          </p>
-          <input
-            className="form-control"
-            type="text"
-            placeholder="Test for focus"
-            onFocus={() => console.log('Input 1 focused')}
-          />
-          <p>
-            From the moment of the first signs of it, a man would be dead in an
-            hour. Some lasted for several hours. Many died within ten or fifteen
-            minutes of the appearance of the first signs.
-          </p>
-          <p>
-            The heart began to beat faster and the heat of the body to increase.
-            Then came the scarlet rash, spreading like wildfire over the face
-            and body. Most persons never noticed the increase in heat and
-            heart-beat, and the first they knew was when the scarlet rash came
-            out. Usually, they had convulsions at the time of the appearance of
-            the rash. But these convulsions did not last long and were not very
-            severe. If one lived through them, he became perfectly quiet, and
-            only did he feel a numbness swiftly creeping up his body from the
-            feet. The heels became numb first, then the legs, and hips, and when
-            the numbness reached as high as his heart he died. They did not rave
-            or sleep. Their minds always remained cool and calm up to the moment
-            their heart numbed and stopped. And another strange thing was the
-            rapidity of decomposition. No sooner was a person dead than the body
-            seemed to fall to pieces, to fly apart, to melt away even as you
-            looked at it. That was one of the reasons the plague spread so
-            rapidly. All the billions of germs in a corpse were so immediately
-            released.
-          </p>
-        </div>
+        <DemoContent index={1} />
       </AnimateHeight>
 
       <h3>Demo, starting height = auto</h3>
@@ -92,21 +96,17 @@ const Example = () => {
       </p>
       <p className="">Set height to:</p>
       <div className="buttons">
-        <button className="btn btn-sm" onClick={() => setHeight2(0)}>
-          0
-        </button>
-        <button className="btn btn-sm" onClick={() => setHeight2(100)}>
-          100
-        </button>
-        <button className="btn btn-sm" onClick={() => setHeight2(200)}>
-          200
-        </button>
-        <button className="btn btn-sm" onClick={() => setHeight2(300)}>
-          300
-        </button>
-        <button className="btn btn-sm" onClick={() => setHeight2('auto')}>
-          auto
-        </button>
+        {options.map((option) => {
+          const isActive = height2 === option;
+          return (
+            <button
+              className={`btn btn-sm ${isActive ? 'btn-selected' : ''}`}
+              onClick={() => setHeight2(option)}
+            >
+              {option}
+            </button>
+          );
+        })}
       </div>
       <AnimateHeight
         height={height2}
@@ -126,50 +126,7 @@ const Example = () => {
         className="demo demo-2"
         animateOpacity
       >
-        <div className="demo-content">
-          <p>
-            It looked serious, but we in California, like everywhere else, were
-            not alarmed. We were sure that the bacteriologists would find a way
-            to overcome this new germ, just as they had overcome other germs in
-            the past. But the trouble was the astonishing quickness with which
-            this germ destroyed human beings, and the fact that it inevitably
-            killed any human body it entered. No one ever recovered. There was
-            the old Asiatic cholera, when you might eat dinner with a well man
-            in the evening, and the next morning, if you got up early enough,
-            you would see him being hauled by your window in the death-cart. But
-            this new plague was quicker than that&mdash;much quicker.
-          </p>
-          <input
-            className="form-control"
-            type="text"
-            placeholder="Test for focus"
-            onFocus={() => console.log('Input 2 focused')}
-          />
-          <p>
-            From the moment of the first signs of it, a man would be dead in an
-            hour. Some lasted for several hours. Many died within ten or fifteen
-            minutes of the appearance of the first signs.
-          </p>
-          <p>
-            The heart began to beat faster and the heat of the body to increase.
-            Then came the scarlet rash, spreading like wildfire over the face
-            and body. Most persons never noticed the increase in heat and
-            heart-beat, and the first they knew was when the scarlet rash came
-            out. Usually, they had convulsions at the time of the appearance of
-            the rash. But these convulsions did not last long and were not very
-            severe. If one lived through them, he became perfectly quiet, and
-            only did he feel a numbness swiftly creeping up his body from the
-            feet. The heels became numb first, then the legs, and hips, and when
-            the numbness reached as high as his heart he died. They did not rave
-            or sleep. Their minds always remained cool and calm up to the moment
-            their heart numbed and stopped. And another strange thing was the
-            rapidity of decomposition. No sooner was a person dead than the body
-            seemed to fall to pieces, to fly apart, to melt away even as you
-            looked at it. That was one of the reasons the plague spread so
-            rapidly. All the billions of germs in a corpse were so immediately
-            released.
-          </p>
-        </div>
+        <DemoContent index={2} />
       </AnimateHeight>
 
       <h3>Demo, with transition delay</h3>
@@ -184,42 +141,42 @@ const Example = () => {
       </p>
       <p>Set delay to:</p>
       <div className="buttons">
-        <button className="btn btn-sm" onClick={() => setDelay(0)}>
-          0 (default)
-        </button>
-        <button className="btn btn-sm" onClick={() => setDelay(300)}>
-          300
-        </button>
-        <button className="btn btn-sm" onClick={() => setDelay(600)}>
-          600
-        </button>
-        <button className="btn btn-sm" onClick={() => setDelay(1000)}>
-          1000
-        </button>
+        {delays.map((option) => {
+          return (
+            <button
+              className={`btn btn-sm ${delay === option ? 'btn-selected' : ''}`}
+              onClick={() => setDelay(option)}
+            >
+              {option}
+            </button>
+          );
+        })}
       </div>
       <p className="">Set height to:</p>
       <div className="buttons">
-        <button className="btn btn-sm" onClick={() => setHeight3(0)}>
-          0
-        </button>
-        <button className="btn btn-sm" onClick={() => setHeight3(100)}>
-          100
-        </button>
-        <button className="btn btn-sm" onClick={() => setHeight3(200)}>
-          200
-        </button>
-        <button className="btn btn-sm" onClick={() => setHeight3(300)}>
-          300
-        </button>
-        <button className="btn btn-sm" onClick={() => setHeight3('auto')}>
-          auto
-        </button>
+        {options.map((option) => {
+          const isActive = height3 === option;
+          return (
+            <button
+              className={`btn btn-sm ${isActive ? 'btn-selected' : ''}`}
+              onClick={() => setHeight3(option)}
+            >
+              {option}
+            </button>
+          );
+        })}
         <br />
-        <button className="btn btn-sm" onClick={() => setHeight3('50%')}>
-          50% (of the parent height)
+        <button
+          className={`btn btn-sm ${height3 === '50%' ? 'btn-selected' : ''}`}
+          onClick={() => setHeight3('50%')}
+        >
+          50% of the parent's height
         </button>
-        <button className="btn btn-sm" onClick={() => setHeight3('100%')}>
-          100% (of the parent height)
+        <button
+          className={`btn btn-sm ${height3 === '100%' ? 'btn-selected' : ''}`}
+          onClick={() => setHeight3('100%')}
+        >
+          100% of the parent's height
         </button>
       </div>
       <div className="demo-3-wrapper">
@@ -241,51 +198,7 @@ const Example = () => {
             );
           }}
         >
-          <div className="demo-content">
-            <p>
-              It looked serious, but we in California, like everywhere else,
-              were not alarmed. We were sure that the bacteriologists would find
-              a way to overcome this new germ, just as they had overcome other
-              germs in the past. But the trouble was the astonishing quickness
-              with which this germ destroyed human beings, and the fact that it
-              inevitably killed any human body it entered. No one ever
-              recovered. There was the old Asiatic cholera, when you might eat
-              dinner with a well man in the evening, and the next morning, if
-              you got up early enough, you would see him being hauled by your
-              window in the death-cart. But this new plague was quicker than
-              that&mdash;much quicker.
-            </p>
-            <input
-              className="form-control"
-              type="text"
-              placeholder="Test for focus"
-              onFocus={() => console.log('Input 2 focused')}
-            />
-            <p>
-              From the moment of the first signs of it, a man would be dead in
-              an hour. Some lasted for several hours. Many died within ten or
-              fifteen minutes of the appearance of the first signs.
-            </p>
-            <p>
-              The heart began to beat faster and the heat of the body to
-              increase. Then came the scarlet rash, spreading like wildfire over
-              the face and body. Most persons never noticed the increase in heat
-              and heart-beat, and the first they knew was when the scarlet rash
-              came out. Usually, they had convulsions at the time of the
-              appearance of the rash. But these convulsions did not last long
-              and were not very severe. If one lived through them, he became
-              perfectly quiet, and only did he feel a numbness swiftly creeping
-              up his body from the feet. The heels became numb first, then the
-              legs, and hips, and when the numbness reached as high as his heart
-              he died. They did not rave or sleep. Their minds always remained
-              cool and calm up to the moment their heart numbed and stopped. And
-              another strange thing was the rapidity of decomposition. No sooner
-              was a person dead than the body seemed to fall to pieces, to fly
-              apart, to melt away even as you looked at it. That was one of the
-              reasons the plague spread so rapidly. All the billions of germs in
-              a corpse were so immediately released.
-            </p>
-          </div>
+          <DemoContent index={3} />
         </AnimateHeight>
       </div>
     </div>
