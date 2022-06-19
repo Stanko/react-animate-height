954c742243bb5bb6a524a3fe7879255f923cfe8c
Stanko <stanko.tadic@gmail.com>
2022-06-19 09:08:02 +0200

    Updated dependencies

---
+++ cypress.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'cypress'
+
+export default defineConfig({
+  video: false,
+  screenshotOnRunFailure: false,
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require('./cypress/plugins/index.js')(on, config)
+    },
+  },
+})
