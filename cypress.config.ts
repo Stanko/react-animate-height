09d35e9767fbda264dca002e7d4917c499c09071
Stanko <stanko.tadic@gmail.com>
2022-06-23 09:45:45 +0200

    Removed unused files

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,13 +1,7 @@
-import { defineConfig } from 'cypress'
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
   video: false,
   screenshotOnRunFailure: false,
-  e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
-    setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config)
-    },
-  },
-})
+  e2e: {},
+});
